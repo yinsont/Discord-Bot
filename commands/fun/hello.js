a68deac0cb0713a1640292f5b5c5cd8da2f4e845
Yinson Tso <yinson.tso@gmail.com>
2023-06-16 12:37:21 -0400

    working pings

--- commands/fun/hello.js
+++ commands/fun/hello.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder} = require('discord.js')
+const {SlashCommandBuilder, messageLink} = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,6 +7,10 @@ module.exports = {
         .addUserOption(option => option.setName('target').setDescription('The user you wish to say hello to')),
     async execute(interaction) {
         const member = interaction.options.getMember('target')
-        return interaction.reply(`${interaction.user.username} says hello to ${member.user.username}`)
+        // console.log(member.user)
+        // console.log('------------------------------------------------------------------')
+        // console.log(interaction.user)
+        // return interaction.reply(`${interaction.user.username} says hello to @${member.user.username}`)
+        return interaction.reply(`<@${interaction.user.id}> says hello to <@${member.user.id}>`)
     }
 }
