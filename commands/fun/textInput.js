4dcce2e0b8ba94dff907f76d4fc6d651e5795352
Yinson Tso <yinson.tso@gmail.com>
2023-06-21 13:35:21 -0400

    working chat gpt

---
+++ commands/fun/textInput.js
@@ -0,0 +1,14 @@
+const {SlashCommandBuilder} = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('text')
+        .setDescription('testing')
+        .addStringOption(option => option.setName('string').setDescription('test')),
+    async execute(interaction){
+        const str = interaction.options.getString('string')
+
+        console.log(str)
+        return interaction.reply({ content: 'An attempt was made', ephemeral: true})
+    }
+}
