ccca84662a62e89c1986ad3478653c8d06c34c3b
Yinson Tso <yinson.tso@gmail.com>
2023-06-26 14:33:45 -0400

    bot auto leaves

--- commands/audio/music.js
+++ commands/audio/music.js
@@ -51,7 +51,7 @@ module.exports = {
 // subscription could be undefined if the connection is destroyed!
     if (subscription) {
 	// Unsubscribe after 5 seconds (stop playing audio on the voice connection)
-	setTimeout(() => subscription.unsubscribe(), 5_000);
+	setTimeout(() => connection.destroy(), 3_000);
     }
   },
 }
