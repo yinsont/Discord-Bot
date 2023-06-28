9108f24d107e88067a0bd9fd6338c6eb8e6e55ea
Yinson Tso <yinson.tso@gmail.com>
2023-06-28 13:28:21 -0400

    converts text to speech to file

--- commands/tts/tts.js
+++ commands/tts/tts.js
@@ -1,4 +1,6 @@
 const {SlashCommandBuilder, messageLink, Message} = require('discord.js')
+const say = require('say')
+const gTTS = require('gtts');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,7 +12,14 @@ module.exports = {
     async execute(interaction) {
         const msg = interaction.options.getString('message')
         //?1118555076794003559
-        await interaction.channel.send({ content: '/tts ' + msg, tts: true})
+        
+        let gtts = new gTTS(msg, 'en');
+        gtts.save('./commands/tts/hello.mp3', function (err, result) {
+          if(err) { throw new Error(err) }
+          console.log('Success! Open file /tmp/hello.mp3 to hear result.');
+        });
+          
+        interaction.channel.send(msg)
     }
 }//https://www.edenai.co/post/top-10-text-to-speech-api 
 //https://murf.ai/text-to-speech-api
